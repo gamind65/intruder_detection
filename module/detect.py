768dcfa368a175482a272e9297888ff688e98061
GaMinD <tanpesi123@gmail.com>
2024-06-17 16:13:19 +0700

    update count, fps, input frame

--- module/detect.py
+++ module/detect.py
@@ -1,16 +1,14 @@
 import cv2
 import numpy as np
-import datetime
-import threading
 from ultralytics import YOLO
-from PIL import Image
+import time
 
 # find if a point is inside a polygon
 def isInside(contour, corr):
     return True if cv2.pointPolygonTest(contour, corr, False) == 1 else False
 
 class YoloDetect():
-    def __init__(self, model_path, poly, video_cap, conf_thresh=0.5):
+    def __init__(self, model_path, poly, conf_thresh=0.5):
         """
         Parameters:
         model_path: path to YOLO-v8 model
@@ -20,17 +18,17 @@ class YoloDetect():
         """
         self.model = YOLO(model_path) # load model
         self.poly = np.array(poly, np.int32).reshape(-1,1,2) # points' corrdinate of polygon
-        self.cap = video_cap # object video capture 
         self.conf_threshold = conf_thresh # confidence threshold
         self.count = 0 # count number of people inside the zone 
         self.classes = 0 # class index of 'people' for YOLO
+        self.prev_time = time.time() #
         
     # detect people using YOLO-v8
     def predict(self, img):
         return self.model.predict(img, classes=self.classes, conf=self.conf_threshold)
     
     # give warning with given condition
-    def warn(self, curr):
+    def detect_warn(self, curr):
         if curr == 0:
             if self.count != curr:
                 print("Only person left!")
@@ -41,56 +39,91 @@ class YoloDetect():
                 print("One person out!")
 
     # run function
-    def detect(self):
-        assert(self.cap.isOpened())
+    def detect(self, frame):
+        # read time and show fps
+        curr_time = time.time()
+        fps = str(int(1/(curr_time-self.prev_time)))
+        cv2.putText(frame, f'fps: {fps}', (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+        self.prev_time = curr_time
         
-        try:
-            while True: 
-                # Capture each frame 
-                ret, frame = self.cap.read() 
-                if not ret:
-                    print("Capture failed")
-                    break
+        results = self.predict(frame)
+        
+        # draw polygon
+        frame = cv2.polylines(frame, [self.poly], True, (0,255,0), 3) # img, [poly], isClosed, color, thickness
+        
+        current_count = 0 # count number of poeple currently inside of polygon  
+        for result in results:
+            # iterates through boxes
+            for box in result.boxes:
+                # get corrdinates of the box
+                xA, yA, xB, yB =map(int, box.xyxy[0]) # (left, top, right, bottom) 
                 
-                # get YOLO-v8 prediction
-                results = self.predict(frame)
+                # calculate the coordinates of center bottom of the box 
+                corr = (int((xA+xB)/2), yB) 
+                # cv2.circle(frame, corr, radius=5, color=(255,0,0), thickness=3) # draw the point
                 
-                # draw polygon
-                frame = cv2.polylines(frame, [self.poly], True, (0,255,0), 3) # img, [poly], isClosed, color, thickness
+                # change color of bounding box to red if the person detected is inside and raise current count
+                if isInside(self.poly, corr): 
+                    color = (0,0,255)
+                    current_count += 1
+                # else set default bounding box color to blue
+                else: color = (255,0,0) 
                 
-                current_count = 0 # count number of poeple currently inside of polygon  
-                for result in results:
-                    # iterates through boxes
-                    for box in result.boxes:
-                        # get corrdinates of the box
-                        xA, yA, xB, yB =map(int, box.xyxy[0]) # (left, top, right, bottom) 
-                        
-                        # calculate the coordinates of center bottom of the box 
-                        corr = (int((xA+xB)/2), yB) 
-                        # cv2.circle(frame, corr, radius=5, color=(255,0,0), thickness=3) # draw the point
-                        
-                        # change color of bounding box to red if the person detected is inside and raise current count
-                        if isInside(self.poly, corr): 
-                            color = (0,0,255)
-                            current_count += 1
-                        # else set default bounding box color to blue
-                        else: color = (255,0,0) 
-                        
-                        # draw bounding box
-                        cv2.rectangle(frame, (xA,yA), (xB,yB), color, 3)
-                    
-                # give warning message and update counter
-                self.warn(current_count)
-                self.count = current_count
-                    
-                # draw frame
-                img = np.array(frame)
-                cv2.namedWindow('Intruder Detection', cv2.WINDOW_NORMAL)
-                cv2.resizeWindow('Intruder Detection', 960, 590) 
-                cv2.imshow('Intruder Detection', img)
-                
-                if cv2.waitKey(1) == 27:  # ESC key to break
-                    break    
-        finally:
-            cv2.destroyAllWindows()
+                # draw bounding box
+                cv2.rectangle(frame, (xA,yA), (xB,yB), color, 3)
             
+        # give warning message and update counter
+        self.detect_warn(current_count)
+        self.count = current_count
+        cv2.putText(frame, f'count: {self.count}', (30,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+        
+        # draw frame
+        img = np.array(frame)
+        cv2.namedWindow('Intruder Detection', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Intruder Detection', 960, 590) 
+        cv2.imshow('Intruder Detection', img)
+        
+    def track(self, frame):
+        # read time and show fps
+        curr_time = time.time()
+        fps = str(int(1/(curr_time-self.prev_time)))
+        cv2.putText(frame, f'fps: {fps}', (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+        self.prev_time = curr_time
+        
+        results = self.model.track(frame, conf=0.5, iou=0.5, classes=self.classes)
+        
+        # draw polygon
+        frame = cv2.polylines(frame, [self.poly], True, (0,255,0), 3) # img, [poly], isClosed, color, thickness
+        
+        current_count = 0 # count number of poeple currently inside of polygon  
+        for result in results:
+            # iterates through boxes
+            for box in result.boxes:
+                # get corrdinates of the box
+                xA, yA, xB, yB =map(int, box.xyxy[0]) # (left, top, right, bottom) 
+                
+                # calculate the coordinates of center bottom of the box 
+                corr = (int((xA+xB)/2), yB) 
+                # cv2.circle(frame, corr, radius=5, color=(255,0,0), thickness=3) # draw the point
+                
+                # change color of bounding box to red if the person detected is inside and raise current count
+                if isInside(self.poly, corr): 
+                    color = (0,0,255)
+                    current_count += 1
+                # else set default bounding box color to blue
+                else: color = (255,0,0) 
+                
+                # draw bounding box
+                cv2.rectangle(frame, (xA,yA), (xB,yB), color, 3)
+            
+        # give warning message and update counter
+        self.detect_warn(current_count)
+        self.count = current_count
+        cv2.putText(frame, f'count: {self.count}', (30,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            
+        # draw frame
+        img = np.array(frame)
+        cv2.namedWindow('Intruder Detection', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Intruder Detection', 960, 590) 
+        cv2.imshow('Intruder Detection', img)
+    
--- test.py
+++ test.py
@@ -1,13 +1,33 @@
-from utils import YoloDetect
+from module.detect import *
 import json
 
+def test(frame, yo, mode=0):
+        if mode == 0: yo.track(frame)
+        else: yo.detect(frame)
+
 def main():
+    # setup 
     with open("config.json", "r") as f:
         config = json.load(f)
-    
+    yo = YoloDetect(model_path=config['model_path'], poly=config['poly'], conf_thresh=config['conf_thresh'])
     cap = cv2.VideoCapture(config['cam_idx'])
-    yo = YoloDetect(model_path=config['model_path'], poly=config['poly'], video_cap=cap, conf_thresh=config['conf_thresh'])
-    yo.detect()
+    assert(cap.isOpened()==True)
+    
+    # loop to get frames and predict
+    while 1:
+        ret, frame = cap.read()
+        
+        if not ret: 
+            break
+        
+        # perform track
+        test(frame, yo, 1)
+        
+        key = cv2.waitKey(1)
+        if key == ord('q'): break
+        
+    # Release video sources
+    cap.release()
     
 if __name__ == "__main__":
     main()
